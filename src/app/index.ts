b66ffd0dfeb44d4a17939dc4b36d1ec12693c476
Manish Prasad <prasad.bmanish999@gmail.com>
2024-03-25 23:21:28 +0530

    added init server

---
+++ src/app/index.ts
@@ -0,0 +1,12 @@
+import express from 'express'
+import { ApolloServer } from '@apollo/server'
+import {expressMiddleware} from '@apollo/server/express4'
+
+export async function initServer(){
+    const app = express();
+    const graphqlServer  = new ApolloServer({typeDefs:'', resolvers:{}})
+    await graphqlServer.start()
+    app.use('/graphql' , expressMiddleware(graphqlServer))
+    return app;
+}
+
